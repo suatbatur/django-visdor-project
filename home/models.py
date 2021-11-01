5cd32f80fed4ec2576d36a9cfc184a34a6f371c5
SuatBatur <msuatbatur@gmail.com>
2021-11-01 21:42:20 +0300

    panel and templates added

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=250)
+    email = models.CharField(max_length=254)
+    message = models.CharField(max_length=1000)
+    created_date = models.DateTimeField( auto_now_add=True)
+    
+    def __str__(self) :
+        return self.name
+    
+    
+        
--- main/settings.py
+++ main/settings.py
@@ -141,7 +141,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #heroku için
 
 MEDIA_URL = '/media/'
 
