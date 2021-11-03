a6b263680e6e060dc17fac2a80e1027e5f68bd30
SuatBatur <msuatbatur@gmail.com>
2021-11-03 16:14:07 +0300

    bg bug fixed

--- main/settings.py
+++ main/settings.py
@@ -88,17 +88,17 @@ DATABASES = {
     }
 }
 
-#DATABASES = {
- #   "default": {
-  #      'ENGINE': 'django.db.backends.postgresql_psycopg2',
-   #     "NAME": config("SQL_DATABASE"),
-    #    "USER": config("SQL_USER"),
-     #   "PASSWORD": config("SQL_PASSWORD"),
-      #  "HOST": config("SQL_HOST"),
-      #  "PORT": config("SQL_PORT"),
-       # "ATOMIC_REQUESTS": True,
-    #}
-#}
+# DATABASES = {
+#    "default": {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        "NAME": config("SQL_DATABASE"),
+#        "USER": config("SQL_USER"),
+#        "PASSWORD": config("SQL_PASSWORD"),
+#        "HOST": config("SQL_HOST"),
+#        "PORT": config("SQL_PORT"),
+#        "ATOMIC_REQUESTS": True,
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
