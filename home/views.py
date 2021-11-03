f26d1186fbfcfe9885251c63b358e10a3b1f19e0
SuatBatur <msuatbatur@gmail.com>
2021-11-03 21:53:43 +0300

    repo updated

--- home/views.py
+++ home/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from .forms import ContactForm
+from django.contrib import messages
+
+
 
 
 # Create your views here.
 
 def home(request):
-    return render(request, "home/index.html")
+    
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Form has been sent")
+            return redirect("home")
+    else:
+        form = ContactForm()
+        
+    context = {
+        'form':form
+    }
+    return render(request, "home/index.html", context)
 
 def about(request):
     return render(request, "home/about.html")
